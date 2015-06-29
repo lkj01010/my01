6fdd53a18c0210ec6a34df3d7173e4ef5579f9c7
LinKejun <86190827@qq.com>
2015-06-29 16:31:42 +0800

    first commit

---
+++ src/task.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "common.h"
+
+namespace detail{
+	template<class T>
+	struct task_wrapped{
+	private:
+		T task_unwrapped_;
+	public:
+		explicit task_wrapped(const T& task_unwrapped)
+			: task_unwrapped_(task_unwrapped)
+		{}
+
+		void operator()() const {
+			try{
+				boost::this_thread::interruption_point();
+			}
+			catch (const boost::thread_interrupted&){}
+
+			try{
+				task_unwrapped_();
+			}
+			catch (const std::exception& e){ std::cerr << "Exception: " << e.what() << '\n'; }
+			catch (const boost::thread_interrupted&){ std::cerr << "Thread interrupted\n"; }
+			catch (...){ std::cerr << "Unknown exception\n"; }
+		}
+	};
+
+	template<class T>
+	task_wrapped<T> make_task_wrapped(const T& task_unwrapped){
+		return task_wrapped<T>(task_unwrapped);
+	}
+
+}
+
+
+class tasks_processor : private boost::noncopyable{
+	boost::asio::io_service	ios_;
+	boost::asio::io_service::work	work_;
+	tasks_processor()
+		: ios_()
+		, work_(ios_)
+	{}
+public:
+	static tasks_processor& get(){
+		return s_inst_;
+	}
+
+	template<class T>
+	inline void push_task(const T& task_unwrapped){
+		ios_.post(detail::make_task_wrapped(task_unwrapped));
+	}
+
+	void start(){
+		ios_.run();
+	}
+
+	void stop(){
+		ios_.stop();
+	}
+
+private:
+	static tasks_processor s_inst_;
+};
+
+
+
+
+
+
+
